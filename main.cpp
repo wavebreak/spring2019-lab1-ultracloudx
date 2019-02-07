37cf9f26eb3535dce12ab9b7f506ac26fd97f4b6
ultracloudx <alexander.ta2007@gmail.com>
2019-02-07 10:49:10 -0600

    updating

--- main.cpp
+++ main.cpp
@@ -13,9 +13,25 @@ int main() {
     getline(cin, userLine);
     cout << "You entered: " << userLine << endl;
 
-
+    if (userLine.find("IDK") != string::npos) {
+        userLine = userLine.replace(userLine.find("IDK"), 3, "I don't know");
+    }
+    else if (userLine.find("BFF") != string::npos) {
+        userLine = userLine.replace(userLine.find("BFF"), 3, "best friend forever");
+    }
+    else if (userLine.find("JK") != string::npos) {
+        userLine = userLine.replace(userLine.find("JK"), 2, "just kidding");
+    }
+    else if (userLine.find("TMI") != string::npos) {
+        userLine = userLine.replace(userLine.find("TMI"), 3, "too much information");
+    }
+    else if (userLine.find("TTYL") != string::npos) {
+        userLine = userLine.replace(userLine.find("TTYL"), 4, "talk to you later");
+    }
 
     cout << "Expanded: " << userLine << endl;
+
+
 
 
     return 0;
